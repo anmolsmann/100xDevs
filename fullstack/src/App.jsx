626f019be70b1b3becc37d62472e6f8c7527f6d7
Anmol Preet Singh <anmolpreet2528@gmail.com>
2024-02-01 16:04:07 +0530

    Additions

--- fullstack/src/App.jsx
+++ fullstack/src/App.jsx
@@ -1,18 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <CustomButton count ={count} setCount={setCount}></CustomButton>
     </div>
   )
+}
+
+
+//component 
+function CustomButton(props){
+
+  function onClickHandler(){
+    props.setCount(props.count+1)
+  }
+
+  return <button onClick={onClickHandler}>Counter {props.count}</button>
 }
 
 export default App
