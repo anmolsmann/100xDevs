487c3c30a5fc8fedfe675c6a214286d164897832
Anmol Preet Singh <anmolpreet2528@gmail.com>
2024-02-01 17:53:52 +0530

    Additions

--- fullstack/src/App.jsx
+++ fullstack/src/App.jsx
@@ -1,24 +1,44 @@
 import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState([{
+    title: "Go to the gym",
+    description: "5-7",
+    completed: false
+  },{
+    title: "prep for the gre",
+    description: '8-9',
+    completed: true
+  }])
+
+  function addTodo(){
+    //... basically means that it takes the new todos and appends it to the old todos
+    setTodo([...todo, {
+      title: "New todo",
+      description: "Description of the new todo"
+    }])
+  }
+
 
   return (
     <div>
-      <CustomButton count ={count} setCount={setCount}></CustomButton>
+      {/*<Todo title= {todo[0].title} description={todo[0].description}></Todo>
+      <Todo title= {todo[1].title} description={todo[1].description}></Todo>*/}
+      <button onClick={addTodo}>Add a random ToDo</button>
+    {todo.map(function(todo){
+      return <Todo title={todo.title} description={todo.description}></Todo>
+    })}
     </div>
   )
 }
 
 
 //component 
-function CustomButton(props){
-
-  function onClickHandler(){
-    props.setCount(props.count+1)
-  }
-
-  return <button onClick={onClickHandler}>Counter {props.count}</button>
+function Todo(props){
+  return <div>
+    <h1>{props.title}</h1>
+    <h2>{props.description}</h2>    
+    </div>
 }
 
 export default App
