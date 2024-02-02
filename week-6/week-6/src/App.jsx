a923bee31dc5d0012275d0580893f52c5c271f68
Anmol Preet Singh <anmolpreet2528@gmail.com>
2024-02-02 11:57:35 +0530

    Additions

---
+++ week-6/week-6/src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return(
+    <div>
+      <header title="Anmol"></header>
+      <header title="Avneet"></header>
+    </div>
+  )
+}
+
+function header({title}){
+  return (
+    <div>
+      {title}
+    </div>
+  )
+}
+
+export default App
