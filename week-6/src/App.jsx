be2dea9e1ee0576754ab3cef6b39174cf30b5a04
Anmol Preet Singh <anmolpreet2528@gmail.com>
2024-02-08 18:14:45 +0530

    additions

--- week-6/src/App.jsx
+++ week-6/src/App.jsx
@@ -1,36 +1,50 @@
 import { useState } from "react"
 
+let counter=4
+
 function App() {
+  const [toDo, setTodo] = useState([{
+    "id": 1,
+    "title": "Go to the gym",
+    "description": "Lightweight"
+  },{
+    "id":2,
+    "title": "Study for the gre",
+    "description": "Three friggin fourty"
+  },{
+    "id": 3,
+    "title": "Read the secret",
+    "description": "We so back"
+  }])
+
+  
+function addTodo(){
+  setTodo([...toDo, {
+    "id":counter++,
+    "title": Math.random(),
+    "description": Math.random()
+  }])
+}
 
   return(
     <div>
-      <HeaderWithButton/>
-      <Header title="Avneet"></Header>
+      <button onClick={addTodo}>Add a todo</button>
+      {toDo.map(todo=> <ToDo key ={todo.id} title={todo.title} description={todo.description}/>)}
     </div>
   )
 }
 
-function HeaderWithButton(){
-  const [title, setTitle] = useState("Anmol")
-  function updateTitle(){
-    setTitle(Math.random())
-  }
+//providing keys is of utmost importance when using array map in react
 
+function ToDo({title, description}){
   return <div>
-    <button onClick={updateTitle}>Update the title</button>
-    <Header title={title}></Header>
-  </div>
-}
-
-//memoization
-//const header = React.memo(function Header({title}){
-//return <div> {title} <div/> })
-
-
-function Header({title}){
-  return <div>
+    <h1>
       {title}
-    </div>
+    </h1>
+    <h5>
+      {description}
+    </h5>
+  </div>
 }
 
 export default App
---
+++ week-6/src/Apptwo.jsx
@@ -0,0 +1,17 @@
+function App(){
+    return <div>
+        <CardWrapper innercomponent= {TextComponent}></CardWrapper>
+    </div>
+}
+
+function TextComponent(){
+    <div>
+        Hey there!
+    </div>
+}
+
+function CardWrapper(){
+
+}
+
+export default App
