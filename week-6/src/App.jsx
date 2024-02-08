b2496127fda5c5e2f63d10f7695707ae489918c8
Anmol Preet Singh <anmolpreet2528@gmail.com>
2024-02-08 16:02:30 +0530

    Additions

--- week-6/src/App.jsx
+++ week-6/src/App.jsx
@@ -1,12 +1,31 @@
+import { useState } from "react"
 
 function App() {
+
   return(
-    <>
-      <Header title="Anmol"></Header>
+    <div>
+      <HeaderWithButton/>
       <Header title="Avneet"></Header>
-    </>
+    </div>
   )
 }
+
+function HeaderWithButton(){
+  const [title, setTitle] = useState("Anmol")
+  function updateTitle(){
+    setTitle(Math.random())
+  }
+
+  return <div>
+    <button onClick={updateTitle}>Update the title</button>
+    <Header title={title}></Header>
+  </div>
+}
+
+//memoization
+//const header = React.memo(function Header({title}){
+//return <div> {title} <div/> })
+
 
 function Header({title}){
   return <div>
