eb7edd94a73477bba54349ebb86bc663745a5abf
Anmol Preet Singh <anmolpreet2528@gmail.com>
2024-04-03 13:40:03 +0530

    additions

--- tailcss/src/App.jsx
+++ tailcss/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   //in tailwind we use className instead of style
   return (
     <>
-      <div style= {{display: 'flex', justifyContent: 'space-around'}}>
+      {/* <div style= {{display: 'flex', justifyContent: 'space-around'}}>
         <div style={{backgroundColor: "red"}}>hi</div>
         <div style={{backgroundColor: "green"}}>hi</div>
         <div style={{backgroundColor: "blue"}}>hi</div>
@@ -17,6 +17,16 @@ function App() {
         <div className='bg-yellow-500'>hi</div>
         <div className='bg-green-500'>hi</div>
         <div className='bg-blue-500'>hi</div>
+      </div> */}
+
+      {/* <div className='grid grid-cols-3'>
+        <div className='bg-red-500'>hello</div>
+        <div className='bg-blue-500'>hello</div>
+        <div className='bg-green-500'>hello</div>
+      </div> */}
+
+      <div className='bg-yellow-500 md:bg-blue-500'>
+        Hello
       </div>
     </>
   )
