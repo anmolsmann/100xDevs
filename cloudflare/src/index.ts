6641ec92bc95695b6207c22cf9dd4f3159ecada5
Anmol Preet Singh <anmolpreet2528@gmail.com>
2024-06-14 23:28:17 +0530

    Updations

--- cloudflare/src/index.ts
+++ cloudflare/src/index.ts
@@ -3,7 +3,9 @@ export interface Env{
 }
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		return Response.json({
+			message: "you did not send a get request"
+		})
 	},
 } satisfies ExportedHandler<Env>;
